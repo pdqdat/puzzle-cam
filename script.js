06261d85ef06bb00e99d411b477d9888def11c6f
Dat Phan <phanduongquocdat273@gmail.com>
2023-11-09 13:15:30 +0700

    feat: finish accessing the camera

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let VIDEO = null;
 let CANVAS = null;
 let CONTEXT = null;
+// specify how much of the screen space the video should take up
+let SCALER = 0.8;
+let SIZE = {
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+};
 
 function main() {
     CANVAS = document.getElementById("myCanvas");
     CONTEXT = CANVAS.getContext("2d");
-
-    CANVAS.width = window.innerWidth;
-    CANVAS.height = window.innerHeight;
 
     // ask for permission to use the camera
     let promise = navigator.mediaDevices.getUserMedia({ video: true });
@@ -24,6 +29,9 @@ function main() {
 
             // when video data is available, update it on the canvas
             VIDEO.onloadeddata = function () {
+                handleResize();
+                // window.addEventListener("resize", handleResize);
+
                 updateCanvas();
             };
         })
@@ -32,8 +40,27 @@ function main() {
         });
 }
 
+function handleResize() {
+    CANVAS.width = window.innerWidth;
+    CANVAS.height = window.innerHeight;
+
+    // find the minimum ratio between screen size & video size
+    let resizer =
+        SCALER *
+        Math.min(
+            window.innerWidth / VIDEO.videoWidth,
+            window.innerHeight / VIDEO.videoHeight
+        );
+
+    // set the SIZE attribute accordingly
+    SIZE.width = VIDEO.videoWidth * resizer;
+    SIZE.height = VIDEO.videoHeight * resizer;
+    SIZE.x = (window.innerWidth - SIZE.width) / 2;
+    SIZE.y = (window.innerHeight - SIZE.height) / 2;
+}
+
 function updateCanvas() {
-    CONTEXT.drawImage(VIDEO, 0, 0, CANVAS.width, CANVAS.height);
+    CONTEXT.drawImage(VIDEO, SIZE.x, SIZE.y, SIZE.width, SIZE.height);
 
     window.requestAnimationFrame(updateCanvas);
 }
